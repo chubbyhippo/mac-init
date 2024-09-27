8f3c2e107e42bed2fe24b7ae9f77c6f34ca2ae74
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-09-27 23:04:40 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -45,6 +45,7 @@ install_brew() {
 }
 
 mac_setup() {
+	xcode-select --install
 	defaults write -g NSWindowShouldDragOnGesture YES
 	curl https://raw.githubusercontent.com/chubbyhippo/aerospace/main/.aerospace.toml -o ~/.aerospace.toml
 }
