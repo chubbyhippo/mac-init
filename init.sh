0942ce0c9837bfa3381efce5c818f1737662ba0c
chubbyhippo <mkmsacc@outlook.com>
2024-09-27 08:08:01 +0700

    Indent init.sh file correctly
    Corrected inconsistent indentation in init.sh to enhance readability and maintain coding standards. This adjustment ensures uniformity in the script, making future amendments and debugging more straightforward.

--- init.sh
+++ init.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env bash
 
 append_to_zshrc() {
-  local text="$1" zshrc
-  local skip_new_line="${2:-0}"
+	local text="$1" zshrc
+	local skip_new_line="${2:-0}"
 
-  zshrc="$HOME/.zshrc"
+	zshrc="$HOME/.zshrc"
 
-  if ! grep -Fqs "$text" "$zshrc"; then
-    if [ "$skip_new_line" -eq 1 ]; then
-      printf "%s\\n" "$text" >> "$zshrc"
-    else
-      printf "\\n%s\\n" "$text" >> "$zshrc"
-    fi
-  fi
+	if ! grep -Fqs "$text" "$zshrc"; then
+		if [ "$skip_new_line" -eq 1 ]; then
+			printf "%s\\n" "$text" >>"$zshrc"
+		else
+			printf "\\n%s\\n" "$text" >>"$zshrc"
+		fi
+	fi
 }
 
 prepend_to_zshrc() {
-  local text="$1" zshrc
+	local text="$1" zshrc
 
-  zshrc="$HOME/.zshrc"
+	zshrc="$HOME/.zshrc"
 
-  printf "%s\n\n%s" "$text" "$(cat "$zshrc")" > "$zshrc"
+	printf "%s\n\n%s" "$text" "$(cat "$zshrc")" >"$zshrc"
 }
 pre_setup() {
-  if [ ! -f "$HOME/.zshrc" ]; then
-    touch "$HOME/.zshrc"
-  fi
-  append_to_zshrc "export PATH=\"$HOME/.bin:$PATH\""
+	if [ ! -f "$HOME/.zshrc" ]; then
+		touch "$HOME/.zshrc"
+	fi
+	append_to_zshrc "export PATH=\"$HOME/.bin:$PATH\""
 }
 
 configure_zsh() {
-  # install oh my zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+	# install oh my zsh
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 }
 
 install_brew() {
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  curl https://raw.githubusercontent.com/chubbyhippo/homebrew/main/Brewfile -o "$HOME/.Brewfile"
-  brew bundle --global
+	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	curl https://raw.githubusercontent.com/chubbyhippo/homebrew/main/Brewfile -o "$HOME/.Brewfile"
+	brew bundle --global
 }
 
 defaults write -g NSWindowShouldDragOnGesture YES
