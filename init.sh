aa01b12e8891f5eb70c0da710dc606cb4f92e6f2
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-08-12 09:58:20 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env sh
 
-configure_zsh() {
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-}
+# ctrl + cmd and click to drag from anywhere
+defaults write -g NSWindowShouldDragOnGesture YES
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	curl https://raw.githubusercontent.com/chubbyhippo/homebrew-brew/refs/heads/main/Brewfile -o "$HOME/Brewfile"
-	brew bundle --global
-
-
-mac_setup() {
-	defaults write -g NSWindowShouldDragOnGesture YES
-}
-
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+curl https://raw.githubusercontent.com/chubbyhippo/homebrew-brew/refs/heads/main/Brewfile -o "$HOME/Brewfile"
+brew bundle --global
