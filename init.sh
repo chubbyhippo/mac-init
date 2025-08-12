881e83a6b7088603b1cbecc61d6c18532721229e
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-08-12 09:56:55 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,21 +4,13 @@ configure_zsh() {
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 }
 
-install_brew() {
+
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	curl https://raw.githubusercontent.com/chubbyhippo/homebrew-brew/refs/heads/main/Brewfile -o "$HOME/Brewfile"
 	brew bundle --global
-	export PATH="/usr/local/opt/curl/bin:$PATH"
-	export PATH="/usr/local/opt/libpq/bin:$PATH"
- 	append_to_zshrc "export PATH=\"$HOME/.bin:$PATH\""	
-}
+
 
 mac_setup() {
 	defaults write -g NSWindowShouldDragOnGesture YES
 }
-
-mac_setup
-pre_setup
-configure_zsh
-install_brew
 
