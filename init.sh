16c76a2f129ae4add5c923154cee1bc58ab5c217
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-08-12 21:13:04 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@
 defaults write com.apple.universalaccess reduceMotion -bool true
 # ctrl + cmd and click to drag from anywhere
 defaults write -g NSWindowShouldDragOnGesture YES
+# autohide dock
+defaults write com.apple.dock "autohide" -bool "true" && killall Dock
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
